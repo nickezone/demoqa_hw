c451e4dd5ef9e49bee7688b28ba8abe6302acb79
Alexandr Tereshko <nickezpra@gmail.com>
2025-02-12 10:56:38 +0300

    Completed PageObjects for FillTest

--- src/test/java/DemoqaHWTests.java
+++ src/test/java/DemoqaHWTests.java
@@ -1,11 +1,5 @@
-import Pages.Components.RegistrationResultModal;
 import Pages.RegistrationPage;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
 
 public class DemoqaHWTests extends TestBase {
 
@@ -17,29 +11,21 @@ public class DemoqaHWTests extends TestBase {
         registrationPage.openPage()
                 .setFirstName("Alex")
                 .setLastName()
-                .setGender("Male");
+                .setGender("Male")
+                .setEmail("alex@egorov.com")
+                .setPhoneNumber("0123456789")
+                .setBirthDate("29", "May", "2001")
+                .setSubject("Maths")
+                .setHobby("Sports")
+                .selectPicture("img/1.png")
+                .setAddress("Miami")
+                .setState("NCR")
+                .setCity(1)
+                .clickSubmitButton();
 
-
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#gender-radio-1").parent().click();
-        $("#genterWrapper").$(byText("Other")).click();
-        $("label[for=gender-radio-1]").click();
-        $("#userNumber").setValue("0123456789");
-
-        $("#subjectsInput").setValue("Maths").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-//        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
-        $("#uploadPicture").uploadFromClasspath("img/1.png");
-        $("#currentAddress").setValue("Miami");
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
-//        $("#react-select-3-option-0").click();
-        $("#city").click();
-        $("#react-select-4-option-1").click();
-        $("#submit").click();
 
         registrationPage.verifyResultModalAppears()
-                .verifyResult("Student name", "Alex" + "Egorov")
+                .verifyResult("Student Name", "Alex " + "Egorov")
                 .verifyResult("Gender", "Male");
     }
 }
